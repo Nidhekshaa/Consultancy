acdca386507fac9f491a7cfe40444b1d67e60f90
Nidhekshaa <nknidhekshaa@gmail.com>
2025-05-23 12:16:45 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -183,6 +183,6 @@ app.get('/', (req, res) => {
 });
 // 🟢 Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on 0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
