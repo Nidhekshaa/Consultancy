2cf45e104da2113e5c0328f2355d1565869a465d
Nidhekshaa <nknidhekshaa@gmail.com>
2025-12-23 19:52:08 +0530

    updated

--- Frontend/src/pages/ResetPassword.jsx
+++ Frontend/src/pages/ResetPassword.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "../styles/Resetpassword.css";
+import "../styles/ResetPassword.css";
 
 export default function ResetPassword() {
   const [password, setPassword] = useState("");
