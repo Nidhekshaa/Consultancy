72ce6a8c48be7ff851cfcb69576e69b8450483f1
Nidhekshaa <nknidhekshaa@gmail.com>
2025-12-24 12:23:16 +0530

    again changed

--- Frontend/src/pages/Living-Room.jsx
+++ Frontend/src/pages/Living-Room.jsx
@@ -45,7 +45,7 @@ function LivingRoom() {
     navigate(token ? "/cart" : "/login");
   };
 
-  const API_URL = import.meta.env.VITE_API_URL;
+  const API_URL = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
     // Fetch products initially
